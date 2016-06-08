2b558cff7f7e9c4cb44b07b54aaffcb821d75387
zcewij <majew030@x-160-94-223-234.lib.umn.edu>
2016-06-08 15:28:29 -0500

    Added custom elements for links

--- elements.php
+++ elements.php
@@ -78,14 +78,14 @@
             'comment' => 'Select from the menu below.',
             'data_type'   => 'Tiny Text',
         ),
-        
+
          array(
             'label' => 'SolrBbox',
             'name'  => 'Apache Solr Bounding Box',
             'description' =>'Bounding box for Solr.',
             'data_type'   => 'Tiny Text',
         ),
-        
+
          array(
             'label' => 'SolrYear',
             'name'  => 'Apache Solr Year',
@@ -93,4 +93,43 @@
             'comment' => 'This is automatically set to the value entered in Temporal Coverage. If you entered a date range, this will be the first year in that range. If you prefer to use a different date, enter it below to override.',
             'data_type'   => 'Tiny Text',
         ),
+
+/*New elements for links that will be added to References - KM*/
+
+        array(
+            'label' => 'Information',
+            'name'  => 'Information Page',
+            'description' =>'Link to a web page with information about the item',
+            'data_type'   => 'Tiny Text',
+        ),
+
+        array(
+            'label' => 'Thumbnail',
+            'name'  => 'Thumbnail',
+            'description' =>'Link to a Thumbnail URL',
+            'data_type'   => 'Tiny Text',
+        ),
+
+        array(
+            'label' => 'DownloadFile',
+            'name'  => 'Download File',
+            'description' =>'Link for a direct file download',
+            'data_type'   => 'Tiny Text',
+        ),
+
+         array(
+            'label' => 'EsriService',
+            'name'  => 'Esri Rest Service',
+            'description' =>'Link to any Esri Rest Service (Map Server, Image Server, Feature Server, etc.)',
+            'data_type'   => 'Tiny Text',
+        ),
+
+         array(
+            'label' => 'WebMapService',
+            'name'  => 'Web Map Service',
+            'description' =>'Link to an open source web map service (WMS)',
+            'data_type'   => 'Tiny Text',
+        ),
+
+
     );
