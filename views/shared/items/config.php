87881c6e5b0fe0f4c6d9e703a15452e243a7ecfd
zcewij <majew030@x-160-94-223-234.lib.umn.edu>
2016-06-08 15:50:34 -0500

    commented out NYU specific code

--- views/shared/items/config.php
+++ views/shared/items/config.php
@@ -5,16 +5,16 @@
 	/*If set to true, records will all contain the Institution specified below for
 	"dct_provenance_s" */
 		$HardCodeInstitution_b = false;
-		
+
 	/* If you don't want to hard code the institution, but default to it whenever an
 	alternative is not provided, set the following to true.*/
-		$DefaultInstitution_b = true;
-		
-	/* The institution. */
+		$DefaultInstitution_b = false;
+
+	/* The institution.
 		$Institution = "NYU";
-	
+	*/
 /* GeoNames API Customizations */
-	
+
 	/* Enter the WEB-ACCESSIBLE paths to the two necessary JS files,
 	and your GeoNames API username (please don't use mine!) */
 		$jeoQueryLoc = "http://submit.nyusdr.net/js/jeoquery.js";
@@ -25,21 +25,21 @@
 	/* Automatically prepend slug with the slugified publisher name?
 	   e.g.: WorldAdministrativeUnits --> ESRI_WorldAdministrativeUnits */
 		$SlugPrependPublisher_b = false;
-		
+
 /* UUID Parsing Options */
 
-	/* Use UUID parsing for handle, or just supply own? 
+	/* Use UUID parsing for handle, or just supply own?
 	   NOTE: UUID parsing only works for NYU
 	   handle.net UUIDs –– this will use the slug (derived or provided by user) to infer the
 	   URL of the .zip file for direct download from NYU FDA */
-		$UUIDParsing_b = true;
+		$UUIDParsing_b = false;
 
 	/* Insert link to direct download zip file (using slug that was provided or derived)?
 
        NOTE: Only set this to true if you are using UUID parsing for NYU FDA identifiers!
        If you are not interested in this feature, you may as well turn off UUID parsing as well;
        this is the only advantage to having UUID parsing enabled, at least so far. */
-		$DirectDownloadLink_b = true;
+		$DirectDownloadLink_b = false;
 
 /* References Options */
 	/* Endpoint URLs for GeoServer and GeoServer workspace names */
@@ -48,13 +48,13 @@
 	/* GeoServer Workspace Names */
 		$GeoserverWorkspacePublic = "nyu_sdr";
 		$GeoserverWorkspaceRestricted = "nyu_sdr";
-		
-		
+
+
 
 /* THE FOLLOWING FIVE VARIABLES ONLY APPLY FOR BROWSE.PHP REQUESTS: */
 	/* Enable record production analytics for API requests handled by browse.php? */
 		$log_b = true;
-		
+
 		/* Location of log for record analytics */
 			$speedlog = "/var/www/html/logs/log.txt";
 
@@ -63,7 +63,7 @@
 
 		/* E-mail origin for analytic reports */
 			$from_email = "me@example.com";
-		
+
 		/* E-mail destination for analytic reports */
 			$to_email = "me@example.com";
 
